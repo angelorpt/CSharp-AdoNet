b8d183448ef59905920336f9a3b2d93299677535
Angelo Rafael Peres Pinto <angelorpt2@gmail.com>
2020-03-17 15:27:43 -0400

    Criando conexão com SQL Server

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.SqlClient;
+using static System.Console;
+
+namespace CSharpAdoNET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connString = "Server=LAPTOP-B2ADTHUP\\SQLEXPRESS;Database=CSharpAdoNet;User Id=sa;Password=root";
+            SqlConnection conn = new SqlConnection(connString);
+
+            WriteLine(conn);
+            ReadKey();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
