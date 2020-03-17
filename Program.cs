bb0d2509cb5b336a39403d1d711834b3b52f8e84
Angelo Rafael Peres Pinto <angelorpt2@gmail.com>
2020-03-17 15:41:42 -0400

    Separando a consulta em método e utilizando o método "using"

--- Program.cs
+++ Program.cs
@@ -8,28 +8,48 @@ namespace CSharpAdoNET
     {
         static void Main(string[] args)
         {
-            string connString = "Server=LAPTOP-B2ADTHUP\\SQLEXPRESS;Database=CSharpAdoNet;User Id=sa;Password=root";
-            SqlConnection conn = new SqlConnection(connString);
+            //SqlConnection conn = new SqlConnection(connString);
 
-            conn.Open();
 
-            SqlCommand cmd = conn.CreateCommand();
-            cmd.CommandText = "SELECT ID, NOME, EMAIL FROM CLIENTES ORDER BY ID";
-
-            SqlDataReader dr = cmd.ExecuteReader();
-
-            while (dr.Read())
-            {
-                WriteLine("ID: {0}", dr["id"].ToString());
-                WriteLine("Nome: {0}", dr["nome"].ToString());
-                WriteLine("E-mail: {0}", dr["email"].ToString());
-                WriteLine("-------------------------------------------------\n");
-            }
-
-            conn.Close();
+            //conn.Close();
+            ListarClientes();
 
             ReadLine();
 
+        }
+
+        static void ListarClientes()
+        {
+
+            string connString = getStringConnection();
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+
+                conn.Open();
+
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT ID, NOME, EMAIL FROM CLIENTES ORDER BY ID";
+
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        WriteLine("ID: {0}", dr["id"].ToString());
+                        WriteLine("Nome: {0}", dr["nome"].ToString());
+                        WriteLine("E-mail: {0}", dr["email"].ToString());
+                        WriteLine("-------------------------------------------------\n");
+                    }
+                }
+
+            }
+
+        }
+
+        static string getStringConnection()
+        {
+            string connString = "Server=LAPTOP-B2ADTHUP\\SQLEXPRESS;Database=CSharpAdoNet;User Id=sa;Password=root";
+            return connString;
         }
     }
 }
